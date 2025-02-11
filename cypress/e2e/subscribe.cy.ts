a98ef4864987e0f3e2dd1444a49e7c62aa98bade
Stanislav Chaban <stch@unumed.com>
2025-02-11 22:14:52 +0200

    How to test forms and custom Cypress commands

---
+++ cypress/e2e/subscribe.cy.ts
@@ -0,0 +1,27 @@
+describe('Newsletter Subscribe Form', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  })
+
+  it('Allows user to subscribe to the email list', () => {
+    const validEmail = 'tom@aol.com'
+    cy.getByData('email-input').type(validEmail);
+    cy.getByData('submit-button').click();
+    cy.getByData('success-message').should('exist').contains(validEmail);
+  })
+
+  it('Does not allow an invalid email address', () => {
+    cy.getByData('email-input').type('wrong@');
+    cy.getByData('submit-button').click();
+    cy.getByData('success-message').should('not.exist');
+  })
+
+  it('Users cannot sing up for our newsletter twice', () => {
+    const email = 'john@example.com';
+    cy.getByData('email-input').type(email);
+    cy.getByData('submit-button').click();
+    cy.getByData('email-input').type(email);
+    cy.getByData('submit-button').click();
+    cy.getByData('server-error-message').should('exist').and('have.css', 'color', 'rgb(198, 43, 52)').contains(email);
+  });
+})
